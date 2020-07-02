f124389e282fc9a8ce9469a5fef97517fd8ba505
Lee <letomok@gmail.com>
2020-07-02 20:39:07 +0900

    Updates

---
+++ precache-manifest.a063015f7e59fed617e5f7188eaea069.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d4363f58785623fca0bf6c0b88ee531",
+    "url": "/blog-reboot/index.html"
+  },
+  {
+    "revision": "3b5cdd2c754aac73912e",
+    "url": "/blog-reboot/static/js/2.efd7376d.chunk.js"
+  },
+  {
+    "revision": "7a2ffcd91b60d94372aedfe44596ab15",
+    "url": "/blog-reboot/static/js/2.efd7376d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6c150734fee7461ed61",
+    "url": "/blog-reboot/static/js/main.254f16e2.chunk.js"
+  },
+  {
+    "revision": "9543c0cf72c3f38da4c8",
+    "url": "/blog-reboot/static/js/runtime-main.e5412e25.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/blog/precache-manifest.c2c58840d199698adbb00f0bd6eba9f4.js"
+  "/blog-reboot/precache-manifest.a063015f7e59fed617e5f7188eaea069.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/blog/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/blog-reboot/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
